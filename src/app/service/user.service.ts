465f613306735c7d006f3ad30b1d9ac809822b53
Alejandro Depetris <aledepetris@outlook.com>
2023-09-15 18:06:59 -0300

    Fixing uri

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private host: string = environment.apiUrl;
+
+  constructor( private http: HttpClient ) {}
+
+  public getUsers(): Observable<User[] | HttpErrorResponse> {
+
+    return this.http.get<User[]>(`${this.host}/user/list`)
+
+  }
+
+
+
 }
