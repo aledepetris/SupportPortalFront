b36e64e8369096ee75d8b5ecbde5227e472cd50a
Alejandro Depetris <aledepetris@outlook.com>
2023-09-16 15:52:42 -0300

    minor fixes and changes in authentication service

--- src/app/guard/authentication.guard.ts
+++ src/app/guard/authentication.guard.ts
@@ -16,7 +16,7 @@ export class AuthenticationGuard implements CanActivate {
   }
 
   private isUserLoggedIn(): boolean {
-    if (this.authenticationService.isLoggedIn()) {
+    if (this.authenticationService.isUserLoggedIn()) {
       return true;
     }
     this.router.navigate(['/login']);
